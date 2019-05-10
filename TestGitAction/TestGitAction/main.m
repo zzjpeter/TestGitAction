55ffe000ca40ad1415e318273aa42a64fdd5836c
zhuzhijia <zhuzhijia@lespark.us>
2019-05-10 15:06:29 +0800

    commit

---
+++ TestGitAction/TestGitAction/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGitAction
+//
+//  Created by 朱志佳 on 2019/5/10.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
