b2e36c2ae3f770f2d513ebf0b1e23ab880c726ed
zhuzhijia <zhuzhijia@lespark.us>
2019-05-10 15:18:43 +0800

    commit

---
+++ TestGitAction/TestGitAction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGitAction
+//
+//  Created by 朱志佳 on 2019/5/10.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
